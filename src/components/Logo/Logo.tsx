3525d5c1fa1e51e083d46909e7024a901edd2020
emejiasbrito <88601791+emejiasbrito@users.noreply.github.com>
2022-06-03 14:53:04 -0400

    feat: create logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import  './style/Logo.css'
+
+const Logo = () => {
+
+    return (
+        <div className="Logo">
+            <div className="logo-container">
+                <span>{'{EM}'}</span>
+            </div>
+        </div>
+    )
+
+}
+
+export default Logo
