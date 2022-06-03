44d6c30d80b555c0e43a641dc4185b387e0508fc
emejiasbrito <88601791+emejiasbrito@users.noreply.github.com>
2022-06-03 14:53:49 -0400

    feat: create navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './style/Navbar.css'
+
+const Navbar = () => {
+
+    return (
+        <div className="Navbar">
+            <div className="navbar-container">
+                <a href="https://www.linkedin.com/in/edward-mejias-brito-b2b89b161/" target='_blank'  rel="noreferrer">
+                    <img src="/img/ico-likedin.png" width={22} alt='ico-likedin'/>
+                </a>
+            </div>
+        </div>
+    )
+
+}
+
+export default Navbar
